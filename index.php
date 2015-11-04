16675078a795492774ca3b699c1575acdf586cae
Drew Demechko <drew_demechko@yahoo.com>
2015-11-04 17:02:35 -0600

    Added Simple Navigation Menu

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--This is the home page of the blog/portfolio website-->
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="Style.css">
+</head>
+
+	<nav>
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/about">About</a>
+			</li>
+			<li>
+				<a href="/projects">Projects</a>
+			</li>
+			<li>
+				<a href="/blog">Blog</a>
+			</li>
+			<li>
+				<a href="/contact">Contact</a>
+			</li>
+		</ul>
+	</nav>
+	
+	<body>
+		
+	</body>
+</html>
