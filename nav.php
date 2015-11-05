8bd533d15066d9f006bdb75cab71921300df3a82
Drew Demechko <drew_demechko@yahoo.com>
2015-11-04 23:03:30 -0600

    Created all pages for the website and updated navigation to a single include php file

---
+++ nav.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>	
+<head>
+	<link rel="stylesheet" type="text/css" href="Style.css">
+</head>
+<!--These links will change once implemented to an actual server-->
+	<nav>
+		<ul>
+			<li>
+				<a href="/blog/blogportfolio">Home</a>
+			</li>
+			<li>
+				<a href="/blog/blogportfolio/about.php">About</a>
+			</li>
+			<li>
+				<a href="/blog/blogportfolio/projects.php">Projects</a>
+			</li>
+			<li>
+				<a href="/blog/blogportfolio/blog.php">Blog</a>
+			</li>
+			<li>
+				<a href="/blog/blogportfolio/contact.php">Contact</a>
+			</li>
+		</ul>
+	</nav>
+</html>
