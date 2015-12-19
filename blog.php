e98a1269b733713dcd8671528fb82470ac2baa08
Drew Demechko <drew_demechko@yahoo.com>
2015-12-18 19:32:11 -0600

    Added emailing functionality, waiting to be tested; Added Search, for a user to type his/her own queries and find specific blogs

--- blog.php
+++ blog.php
@@ -9,26 +9,41 @@
 	include 'dbconnection.php';
 	?>
 	<body>
-	<a action=""></a>
+	<form method="post" action="#">
+		<input type="text" class="input" id="txtSearch" name="txtSearch"/>
+		<input style="float:right-center;" type="submit" name="btnSearch" id="btnSearch" value="Search">
+		</select>
+	</form>
 	<?php
 	//If post is set send form data to database
-	if(!empty($_POST))
+	if(!empty($_POST['txtAddEntry']))
 	{
 	$title = $_POST['txtTitle'];
 	$entry = $_POST['txtAddEntry'];
-	$type = $_POST['lstType'];
 	$currDate = date('Y-m-d');
 	
 	mysqli_query($dbconn,
 	"INSERT INTO blogentries
-	VALUES('$title','$entry','$currDate','$type')");
+	VALUES('$title','$entry','$currDate')");
 	}
 	
 	//Load blog entries from database
+	if(!empty($_POST['txtSearch']))
+	{
+	$criteria = $_POST['txtSearch'];
+	$entries = mysqli_query($dbconn,
+	"SELECT title, date, entry
+	FROM blogentries
+	WHERE title LIKE '%$criteria%'
+	OR entry LIKE '%criteria%'");
+	}
+	else
+	{
 	$entries = mysqli_query($dbconn, 
 	"SELECT title, date, entry
 	FROM blogentries
 	ORDER BY date DESC");
+	}
 	
 	//Fetch all entries and store in temp array rows
 	while($row = mysqli_fetch_array($entries))
@@ -41,12 +56,8 @@
 	
 		<form method="post" action="#">
 		<input type="text" class="input" id="txtTitle" name="txtTitle" value="Enter Title Here"/>
-		<div class="input">Type: <select class="input" name="lstType">
-		<option value="sports">Sports</option>
-		<option value="software">Software</option>
-		</select></div>
 		<textarea class="input" id="txtAddEntry" name="txtAddEntry" rows="20">Start Blogging..</textarea>
-		<input type="submit" name="btnAddEntry" id="btnAddEntry" onclick="" value="Add Entry">
+		<input type="submit" name="btnAddEntry" id="btnAddEntry" value="Add Entry">
 		</form>
 	</body>
 	
--- email.php
+++ email.php
@@ -1,1 +1,25 @@
+<!DOCTYPE html>
 <!--This php file sends an email using information gathered from the contact page form-->
+<html>
+
+	<?php 
+	//This includes the navigation code that will be
+	//included on all pages
+	include 'nav.php';
+	
+	//Send email to Owner of blog
+	if(!empty($_POST))
+	{
+	$ownerEmail = 'j671943@trbvm.com';
+	$name = $_POST['txtName'];
+	$email = $_POST['txtEmail'];
+	$subject = $_POST['txtSubject'];
+	$message = $_POST['txtMessage'];
+	
+	mail($ownerEmail,$subject,'$message\n\nFrom: $name\n$email\n');
+	}
+	?>
+	<body>
+	<h1>Thank you for leaving your feedback or request! <br> I will get back with you as soon as possible! <h1>
+	</body>
+</html>
